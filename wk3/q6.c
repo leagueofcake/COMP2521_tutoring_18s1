2fb19a068725df142add9b50c0fe4719f62f720f
Dominic Fung <domokun997@gmail.com>
2018-03-14 09:13:33 +1100

    Update q6 to be runnable, add Makefile

--- wk3/q6.c
+++ wk3/q6.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 typedef unsigned int Word;
 // which on most systems could more accurately defined as 
 // typedef uint32_t Word;
@@ -5,9 +7,17 @@ typedef unsigned int Word;
 void showBits (Word w) {
     int i;
     Word mask, bit;
+    mask = 1 << 31;
     for (i = 31; i >= 0; i--) {
         bit = (w & mask) >> i;
         printf("%d", bit);
         mask = (mask >> 1);
     }
+    printf("\n");
+}
+
+int main () {
+    Word w = 0xFFFF;
+    showBits(w);
+    return 0;
 }
